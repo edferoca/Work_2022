2cadf26f4b2290b334dc4c38fee550381ba6248d
edferoca <47976279+edferoca@users.noreply.github.com>
2022-03-07 19:33:27 +0100

    primeros cambios

---
+++ web_scraping_1.py
@@ -0,0 +1,33 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+import collections
+collections.Callable = collections.abc.Callable
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = "http://www.meteobalaguer.com/"
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags=soup.find_all('td')
+
+
+for tag in tags:
+    if tag.string ==None:
+        fonts=tag.find_all('font')
+        for font in fonts:
+            if len(font.contents) >=1:
+                print(font.contents[0])
+                #for i in font.contents:
+                #    print(font.contents[i])
+            
+    print(tag.string)
